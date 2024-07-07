bea91ef270f1f514cdc567235dfbbeec1d7368fb
Payam <w1zp3r@gmail.com>
2024-07-07 16:42:32 +0330

    Direction Columns

---
+++ lib/columns.dart
@@ -0,0 +1,44 @@
+// Library
+import 'package:flutter/material.dart';
+
+// Columns
+class Sandbox extends StatelessWidget {
+  const Sandbox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sandbox', style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+        )),
+        backgroundColor: Colors.grey,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            width: 100,
+            color: Colors.red,
+            child: const Text('One'),
+            padding: const EdgeInsets.all(20),
+          ),
+          Container(
+            width: 200,
+            color: Colors.green,
+            child: const Text('Two'),
+            padding: const EdgeInsets.all(20),
+          ),
+          Container(
+            width: 300,
+            color: Colors.purple,
+            child: const Text('Three'),
+            padding: const EdgeInsets.all(20),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 // Library
+//// Columns
+import 'package:fanavaran_app/columns.dart';
+//// Home
 import 'package:fanavaran_app/home.dart';
+//// Material
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MaterialApp(
     home: Scaffold(
-      body: Home()
+      body: Sandbox()
     ),
   ));
 }
-
-// Home Widget
