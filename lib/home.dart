6637479ab073063e0cd5911d82bb96fe56b07872
Payam <w1zp3r@gmail.com>
2024-07-07 18:03:44 +0330

    Adding BG to Login Page

--- lib/home.dart
+++ lib/home.dart
@@ -11,24 +11,32 @@ class Home extends StatelessWidget {
     double screenHeight = MediaQuery.of(context).size.height;
 
     return Container(
-      color: const Color.fromRGBO(0, 59, 191, 1.0),
       width: screenWidth, // 100% width
       height: screenHeight, // 100% height
       padding: const EdgeInsets.all(20), // Padding
+      decoration: BoxDecoration(
+        color: const Color.fromRGBO(255, 255, 255, 1.0), // Background color
+        image: const DecorationImage(
+          image: AssetImage('assets/img/pattern-bg.png'),
+          fit: BoxFit.cover,
+        ),
+      ),
       child: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Image.asset(
               'assets/img/logo.png',
-              height: 50, // Adjust the height as needed
+              height: 150, // Adjust the height as needed
             ),
             const SizedBox(height: 40),  // Space between image and text
             const Text(
               'Welcome to Fanavaran',
               style: TextStyle(
-                color: Colors.white, // Text color
-                fontSize: 24, // Text size
+                color: Color(0xFF1A30D3), // Text color in hex format
+                fontSize: 24,
+                fontFamily: 'Poppins',
+                fontWeight: FontWeight.w500, // Text size
               ),
             ),
           ],
