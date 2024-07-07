f5e49d7d7282d8673bd0f902553bace599498a08
Payam <w1zp3r@gmail.com>
2024-07-07 15:09:20 +0330

    Background and Containers

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,6 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      appBar: AppBar(
-        title: Image.asset(
-          'assets/logo.png',
-          height: 30,  // Adjust the height as needed
-        ),
-        backgroundColor: const Color.fromRGBO(0, 59, 191, 1.0),
-        centerTitle: true,
-      ),
       body: const Home()
     ),
   ));
@@ -22,7 +14,35 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text('Hello, Client');
+    // Get the screen width and height
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
+    return Container(
+      color: const Color.fromRGBO(0, 59, 191, 1.0),
+      width: screenWidth, // 100% width
+      height: screenHeight, // 100% height
+      padding: const EdgeInsets.all(20), // Padding
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/logo.png',
+              height: 50, // Adjust the height as needed
+            ),
+            const SizedBox(height: 40),  // Space between image and text
+            const Text(
+              'Welcome to Fanavaran',
+              style: TextStyle(
+                color: Colors.white, // Text color
+                fontSize: 24, // Text size
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
 
